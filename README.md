ccd55ec0db05135b0e5774765d9b572c58ae4b7b
HaidarMestrah <10120319@mu.edu.lb>
2023-07-13 11:47:58 +0300

    applied tic tac toe features, modified styles and added sound effects

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # TicTacToe
+
+### **Game features:**
+
+- **Names are a must** to enter the game, (if a name is empty you cannot enter)
+- Players can **go for a new round**, **play a new game** (reset score) , **restart game with different players**, and **change sides**.
+- **WIN** and **DRAW** are both handled and detected.
+- The game is **supported with audio effects!**
+
+#
+
+### ENJOY TIC TAC TOE! :)
--- Scripts/game.js
+++ Scripts/game.js
@@ -1,28 +1,50 @@
-const X_PlayerInput = document.getElementById("player-x-input");
-const Y_PlayerInput = document.getElementById("player-y-input");
+var X_PlayerInput = document.getElementById("player-x-input");
+var Y_PlayerInput = document.getElementById("player-y-input");
 
-const startBTN = document.getElementById("startBTN");
-startBTN.addEventListener("click", startGame);
+let startBTN = document.getElementById("startBTN");
+let audio = document.getElementById("audio");
+let winSound = document.getElementById("win_sound");
+var newRoundSound = document.getElementById("newRoundSound");
+var drawAudio = document.getElementById("drawAudio");
 
-const X_Player_Name = document.getElementById("playerXName");
-const Y_Player_Name = document.getElementById("playerYName");
-const X_Score_output = document.getElementById("scoreX");
-const Y_Score_output = document.getElementById("scoreY");
+var X_Player_Name = document.getElementById("playerXName");
+var Y_Player_Name = document.getElementById("playerYName");
+var X_Score_output = document.getElementById("scoreX");
+var Y_Score_output = document.getElementById("scoreY");
 
 let x_score = 0;
 let y_score = 0;
+let gameActive = true;
 
-const newGame = document.getElementById("newGame");
-newGame.addEventListener("click", startNewGame);
-const restartNew = document.getElementById("restartNew");
-restartNew.addEventListener("click", restartWithDifferent);
-const winner = document.getElementById("result");
-const newRound = document.getElementById("newRound");
-newRound.addEventListener("click", startNewRound);
+var newGame = document.getElementById("newGame");
+var restartNew = document.getElementById("restartNew");
+var newRound = document.getElementById("newRound");
+var changeSides = document.getElementById("changeSides");
+var winner = document.getElementById("result");
+
+var gameContainer = document.getElementById("gameContainer");
+var playerNames = document.getElementById("playerNames");
+var gameBoardContainer = document.getElementById("gameBoardContainer");
+var gameBoard = document.getElementById("game-board");
+var resultContainer = document.getElementById("resultContainer");
 
 const O_TEXT = "O";
 const X_TEXT = "X";
 let boxes = Array.from(document.getElementsByClassName("box"));
+startBTN.addEventListener("click", function () {
+  if (X_PlayerInput.value === "" || Y_PlayerInput.value === "") {
+    return false;
+  } else {
+    playerNames.style.display = "none";
+    gameContainer.style.display = "flex";
+    gameBoardContainer.style.display = "flex";
+    X_Player_Name.innerHTML = X_PlayerInput.value;
+    Y_Player_Name.innerHTML = Y_PlayerInput.value;
+    X_Score_output.innerHTML = x_score;
+    Y_Score_output.innerHTML = y_score;
+    boxes.forEach((box) => box.addEventListener("click", boxClicked));
+  }
+});
 
 const winCombinations = [
   [0, 1, 2],
@@ -38,11 +60,10 @@ const winCombinations = [
 let currentPlayer = X_TEXT;
 let spaces = Array(9).fill(null);
 
-const startGame = () => {
-  boxes.forEach((box) => box.addEventListener("click", boxClicked));
-};
-
 function boxClicked(e) {
+  if (!gameActive) {
+    return;
+  }
   const id = e.target.id;
 
   if (!spaces[id]) {
@@ -50,25 +71,102 @@ function boxClicked(e) {
     e.target.innerText = currentPlayer;
 
     if (playerHasWon() !== false) {
-      winner.innerHTML = currentPlayer;
+      gameActive = false;
+      winSound.play();
+
+      let winning_blocks = playerHasWon();
       if (currentPlayer === X_TEXT) {
         x_score += 1;
-      } else {
-        y_score += 1;
+        X_Score_output.innerHTML = x_score;
+        winner.innerHTML = X_PlayerInput.value + " Wins !";
       }
-      X_Score_output.innerHTML = x_score;
-      Y_Score_output.innerHTML = y_score;
-      let winning_blocks = playerHasWon();
-
-      winning_blocks.map(
-        (box) => (boxes[box].style.backgroundColor = winnerIndicator)
-      );
+      if (currentPlayer === O_TEXT) {
+        y_score += 1;
+        Y_Score_output.innerHTML = y_score;
+        winner.innerHTML = Y_PlayerInput.value + " Wins !";
+      }
+      winning_blocks.map((box) => (boxes[box].style.backgroundColor = "Green"));
+      resultContainer.style.display = "flex";
       return;
     }
 
-    currentPlayer = currentPlayer == X_TEXT ? O_TEXT : X_TEXT;
+    currentPlayer = currentPlayer === X_TEXT ? O_TEXT : X_TEXT;
+    audio.play();
+    if (checkForDraw() && playerHasWon !== true) {
+      drawAudio.play();
+      winner.innerHTML = "Draw";
+      resultContainer.style.display = "flex";
+    }
   }
 }
+function checkForDraw() {
+  for (const space of spaces) {
+    if (space === null) {
+      return false;
+    }
+  }
+  return true;
+}
 
-//TODO startGame(), playerHasWon()
-//TODO restartWithDifferent(), startNewGame(), startNewRound()
+function playerHasWon() {
+  for (const condition of winCombinations) {
+    let [a, b, c] = condition;
+
+    if (spaces[a] && spaces[a] == spaces[b] && spaces[a] == spaces[c]) {
+      return [a, b, c];
+    }
+  }
+  return false;
+}
+
+newRound.addEventListener("click", startNewRound);
+
+function startNewRound() {
+  newRoundSound.play();
+  gameActive = true;
+  spaces.fill(null);
+  boxes.forEach((box) => {
+    box.innerText = "";
+    box.style.backgroundColor = "rgb(58, 58, 60)";
+  });
+  currentPlayer = X_TEXT;
+  resultContainer.style.display = "none";
+}
+
+newGame.addEventListener("click", restartNewGame);
+function restartNewGame() {
+  startNewRound();
+  x_score = 0;
+  y_score = 0;
+  X_Score_output.innerHTML = 0;
+  Y_Score_output.innerHTML = 0;
+}
+restartNew.addEventListener("click", restartWithNewPlayers);
+
+function restartWithNewPlayers() {
+  restartNewGame();
+  gameContainer.style.display = "none";
+  resultContainer.style.display = "none";
+  playerNames.style.display = "flex";
+  X_PlayerInput.value = "";
+  Y_PlayerInput.value = "";
+}
+
+changeSides.addEventListener("click", switchPlay);
+
+function switchPlay() {
+  startNewRound();
+  let tempScore = 0;
+  let tempName = 0;
+  tempScore = x_score;
+  x_score = y_score;
+  y_score = tempScore;
+  X_Score_output.innerHTML = x_score;
+  Y_Score_output.innerHTML = y_score;
+
+  tempName = X_PlayerInput.value;
+  X_PlayerInput.value = Y_PlayerInput.value;
+  Y_PlayerInput.value = tempName;
+  Y_Player_Name.innerHTML = Y_PlayerInput.value;
+  X_Player_Name.innerHTML = X_PlayerInput.value;
+}
